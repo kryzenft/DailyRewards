2ea6a05e6847cc94427722b1f2abe84f4501edea
kryzenft <ftkryzen@gmail.com>
2026-08-03 19:35:43 +0530

    Initial commit

---
+++ src/main/java/me/kryzen/dailyRewards/DailyRewards.java
@@ -0,0 +1,16 @@
+package me.kryzen.dailyRewards;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DailyRewards extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
